d0362ccf39b96ca50ac60c3a7a51f1784def03a6
rwu@attune.gshapps.net <E7gS#qsR4wwJP7LGRD2Dev#Svr>
2018-05-29 12:59:48 +0800

    add utility

---
+++ MovingEngine/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingEngine
+{
+	public class Utility
+	{
+		public static double UpdateLimitedNumber(double newValue, double maxValue)
+		{
+			if (newValue < 0) return 0;
+			if (newValue > maxValue) return maxValue;
+			return newValue;
+		}
+	}
+}
